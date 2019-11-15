9b1dbb2472fa8fe646a76248c562cdda6c6e2abe
thibault rahou <trahou33@gmail.com>
2019-11-15 16:58:56 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+puts "Deleting all the ingredients \o/"
+Ingredient.destroy_all
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+list = JSON.parse(user_serialized)
+list["drinks"].each do |drink|
+  Ingredient.create!(name: drink["strIngredient1"])
+end
+puts "the end ingredients"
+
+
